dba0586837b2474323f236a4acacedffecf8ae80
Tharushi <tharushinethma392@gmail.com>
2025-05-27 10:35:09 +0530

    pop method in f2 branch is merge with main

--- Test.java
+++ Test.java
@@ -37,4 +37,23 @@ public class Test
 
 	}
 
+	public int pop()
+	{
+		if(isEmpty())
+		{
+			System.out.println(x:"Stack is underflow");
+			return 0;
+
+
+		}else
+		{
+				int item=stk[top++];
+				return item;
+
+		}
+
+
+	}
+	
+
 }
