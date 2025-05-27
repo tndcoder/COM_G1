a14f187f40ceb9fd4bb79f295c7d9bfd8322aba2
Tharushi <tharushinethma392@gmail.com>
2025-05-27 09:46:48 +0530

    isFull method created

--- Test.java
+++ Test.java
@@ -8,9 +8,19 @@ public class Test
 
 	{
 		stk= new int[10];
-  		top= -1;
+  		
 
 
 
 	}
+
+	public boolean isEmpty()
+	{
+		return top==-1;
+	}
+	public boolean isFull()
+	{
+		return top==stk.length-1;
+	}
+
 }
